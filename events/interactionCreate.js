77a735ba28320181276fd6d7404103166017b78b
Krzysztof Gocek <krzysztof.gocek@protonmail.com>
2021-12-30 23:09:23 +0100

    EOL Cleanup

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,7 +3,7 @@ module.exports = {
   once: false,
   execute: async (interaction) => {
     if (!interaction.isCommand) return;
-    
+
     const command = interaction.client.commands.get(interaction.commandName);
 
     if (!command) return;
