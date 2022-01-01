c3b68da060710cd45317616d68e3b63063817951
Krzysztof Gocek <krzysztof.gocek@protonmail.com>
2022-01-01 22:26:22 +0100

    Remove sync

--- events/ready.js
+++ events/ready.js
@@ -1,14 +1,7 @@
-const {
-  authenticate: authenticateDatabase,
-  synchronize: syncDatabase,
-} = require('../utils/database');
-
 module.exports = {
   name: 'ready',
   once: true,
   execute: (client) => {
     console.info(`Ready. Logged in as ${client.user.tag}`);
-    authenticateDatabase();
-    syncDatabase();
   },
 };
