4df2ce61dd3d9fb81da9a672f55a1d19e99ee42d
Krzysztof Gocek <krzysztof.gocek@protonmail.com>
2022-01-05 23:34:12 +0100

    commenting

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,14 +2,17 @@ module.exports = {
   name: 'interactionCreate',
   once: false,
   execute: async (interaction) => {
+    // Commands only
     if (!interaction.isCommand) return;
 
+    // Get command
     const command = interaction.client.commands.get(interaction.commandName);
-
     if (!command) return;
 
+    // Allow delayed response
     await interaction.deferReply();
 
+    // Execute command and deal with errors within
     try {
       await command.execute(interaction);
     } catch (error) {
--- utils/registerGuildCommands.js
+++ utils/registerGuildCommands.js
@@ -4,14 +4,16 @@ const { Discord } = require('../config/env');
 const Commands = require('../commands');
 
 (async () => {
+  // Get commands from 'commands' folder
   const commands = [];
-
   Commands.forEach((command) => {
     commands.push(command.data.toJSON());
   });
 
+  // Set up Discord REST
   const rest = new REST({ version: 9 }).setToken(Discord.token);
 
+  // Register commands
   try {
     console.info('Started refreshing application (/) commands.');
 
